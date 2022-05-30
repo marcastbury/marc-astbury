dbe73964086f646ecaece7cda6b3d9f03cb1f950
marcastbury <103426086+marcastbury@users.noreply.github.com>
2022-05-30 12:58:18 +0100

    Test Accordion

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ checkbox.addEventListener('change', () => {
 
 //Accordian//
 
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+/*const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
 accordionItemHeaders.forEach(accordionItemHeader => {
     accordionItemHeader.addEventListener("click", event => {
@@ -21,3 +21,19 @@ accordionItemHeaders.forEach(accordionItemHeader => {
         }
     });
 });
+*/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
