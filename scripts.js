af04b4fdc21dff32c933a7d82562b7b48a9671c4
marcastbury <103426086+marcastbury@users.noreply.github.com>
2022-05-30 12:05:26 +0100

    Accordian

--- scripts.js
+++ scripts.js
@@ -4,3 +4,22 @@ checkbox.addEventListener('change', () => {
     // change theme
     document.body.classList.toggle('light');
 });
+
+//Accordian//
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+
+    });
+});
